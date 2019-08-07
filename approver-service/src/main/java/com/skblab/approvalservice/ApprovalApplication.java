4172db60d77afe8b5baf1705d1c739c04201f6a0
Alex Scrobot <scrobot91@gmail.com>
2019-08-07 13:58:14 +0700

    init commit:
    - prepared project structure
    - created micro-services templates
    - created protobuff template

---
+++ approver-service/src/main/java/com/skblab/approvalservice/ApprovalApplication.java
@@ -0,0 +1,14 @@
+package com.skblab.approvalservice;
+
+import com.skblab.leadhandlerservice.LeadsHandlerApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprovalApplication.class, args);
+    }
+
+}
---
+++ leads-api/src/main/java/com/skblab/leadsapi/LeadsApiApplication.java
@@ -0,0 +1,13 @@
+package com.skblab.leadsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeadsApiApplication.class, args);
+    }
+
+}
